a423cbc533670e39fc4af6ca0b375f323a3bf666
theGenebob <theprimalrpg@gmail.com>
2026-05-19 16:30:10 -0500

    Fix Render port handling

--- server/index.js
+++ server/index.js
@@ -303,8 +303,8 @@ app.get('/debug/*path', async (req, res) => {
 
 });
 
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
-
-    console.log(`Server running on http://localhost:${PORT}`);
-
+    console.log(`Server running on port ${PORT}`);
 });
